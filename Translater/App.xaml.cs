f60307c91fdaf1e427d619ddbed5dda19b495d03
vladf <vladf@VFILIMONTSEV>
2024-04-09 23:20:42 +0300

    Add project files.

---
+++ Translater/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Translater
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
